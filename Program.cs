bd36d87c9e8adaf4ebc48ed2e2219dca7bd274c7
matheusjean28 <matheusjean28@gmail.com>
2023-12-01 20:25:42 -0300

    simple chat consumer with .net core & websockets

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Uri serverUri = new Uri("ws://localhost:5146/api/chat"); 
+
+        using (ClientWebSocket clientWebSocket = new())
+        {
+            try
+            {
+                await clientWebSocket.ConnectAsync(serverUri, CancellationToken.None);
+
+                _ = ReceiveMessage(clientWebSocket);
+
+                while (true)
+                {
+                    Console.Write("Text your message! (or 'exit' to get out): ");
+                    string input = Console.ReadLine();
+
+                    if (input.ToLower() == "exit")
+                        break;
+
+                    await SendMessage(clientWebSocket, input);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro: {ex.Message}");
+            }
+            finally
+            {
+                if (clientWebSocket.State == WebSocketState.Open)
+                    await clientWebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing your connection", CancellationToken.None);
+            }
+        }
+    }
+
+    static async Task SendMessage(ClientWebSocket webSocket, string message)
+    {
+        byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+        await webSocket.SendAsync(new ArraySegment<byte>(messageBytes), WebSocketMessageType.Text, true, CancellationToken.None);
+    }
+
+    static async Task ReceiveMessage(ClientWebSocket webSocket)
+    {
+        byte[] buffer = new byte[1024];
+        while (webSocket.State == WebSocketState.Open)
+        {
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+            if (result.MessageType == WebSocketMessageType.Text)
+            {   
+                string receivedMessage = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                Console.WriteLine($"Recived message: {receivedMessage }");
+            }
+        }
+    }
+}
